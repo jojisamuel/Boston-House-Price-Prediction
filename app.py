d5cbb99be908b2c9ae94a4ec0257ccb242dfb89c
jojisamuel <jojisamuel@gmail.com>
2022-12-30 01:45:01 +0530

    port issue

--- app.py
+++ app.py
@@ -36,4 +36,5 @@ def predict():
 
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0",debug=True,port='8080')
+    app.run(host="0.0.0.0",debug=True,port=8080)
+    # app.run(host="127.0.0.1", port=8080,debug=True)
